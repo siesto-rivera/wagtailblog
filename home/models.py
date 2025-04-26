f188eafa42d8348467eeb5af5f5ae92e38928a7b
siesto <siesto.rivera@gmail.com>
2025-04-26 23:35:48 +0900

    Page 에 custom field 추가 & admin 패널에 노출하기

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
 
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
