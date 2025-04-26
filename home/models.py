6191ecc9b6a10651555971887609c1a6fc2493c8
siesto <siesto.rivera@gmail.com>
2025-04-27 08:27:05 +0900

    image crop 방법들

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
+from wagtail.images import get_image_model
 
 from wagtail.models import Page
 
@@ -8,8 +9,16 @@ from wagtail.models import Page
 class HomePage(Page):
     subtitle = models.CharField("부제",max_length=100, blank=True, null=True)
     body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        get_image_model(),  # 'wagtailimages.Image' can be used as a string
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle", read_only=True),
         FieldPanel("body"),
+        FieldPanel("image"),
     ]
